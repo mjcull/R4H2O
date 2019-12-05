00d4b04319507c1ed51b4ee0ce21b1606ae34c6d
Peter Prevos <peter@prevos.net>
2019-12-05 20:48:31 +1100

    Melbourne Water update

--- basicr/quiz.R
+++ basicr/quiz.R
@@ -7,12 +7,12 @@ b <- 0.6
 Cd <- 0.6 # approximation
 
 ## Question 1
-h <- 0.1
+h <- 100 / 1000
 q <- (2/3) * Cd * sqrt(2 * 9.81) * b * h^(3/2)  # m3 / s
 q * 3600 * 24 / 1E3  # ML / day
 
 ## Question 2
-h <- c(.15, .136, .075) # Heights in meters
+h <- c(150, 136, 75) / 1000 # Heights in meters
 q <- (2/3) * Cd * sqrt(2 * 9.81) * b * h^(3/2)
 mean(q) * 1000
 
